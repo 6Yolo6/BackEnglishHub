3e0dc79ce41be943479d5b6ee7931461f0664d43
yolo <2031798793@qq.com>
2024-03-23 23:46:46 +0800

    更新依赖项，修复springboot3对应的mybatisplus包的不一致性

--- src/main/java/com/example/englishhub/EnglishHubApplication.java
+++ src/main/java/com/example/englishhub/EnglishHubApplication.java
@@ -1,9 +1,11 @@
 package com.example.englishhub;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.englishhub.mapper")
 public class EnglishHubApplication {
 
     public static void main(String[] args) {
